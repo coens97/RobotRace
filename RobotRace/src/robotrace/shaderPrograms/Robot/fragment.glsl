faddda993b321679d876232eedcb9655f45aeb4e
Coen Stange <coen.stange@hotmail.com>
2016-12-24 14:31:13 +0100

    + added robot shader (phong)

--- RobotRace/src/robotrace/shaderPrograms/Robot/fragment.glsl
+++ RobotRace/src/robotrace/shaderPrograms/Robot/fragment.glsl
@@ -1,5 +1,40 @@
+//uniform bool ambient, diffuse, specular;
+varying vec3 N, P;
+
+vec4 shading(vec3 P, vec3 N, gl_MaterialParameters mat) {
+ vec4 result = vec4(0.0, 0.0, 0.0, 1.0); // opaque black
+ for(int i=0; i<3;i++){ // loop through the lights
+
+  gl_LightSourceParameters light = gl_LightSource[i];
+
+  
+  //if (ambient) {
+   result += mat.ambient * light.ambient; // add ambient color to sum
+  //}
+
+ 
+
+  vec3 L = normalize(light.position.xyz - P); // vector towards light source
+  float cosTheta = max(0.0, dot(N, L)); // angle between the normal and the light source
+  //if (diffuse) {
+   result += mat.diffuse * light.diffuse * cosTheta; // sum the color of diffuse times the intensity
+  //}
+ 
+  //if (specular) {
+   vec3 E = vec3(0.0); // position of camera in View space
+   vec3 V = normalize(E - P); // direction towards viewer
+   vec3 iL = normalize(-1.0 * L); // vector comming from light source
+   vec3 reflection = normalize(iL - 2.0 * dot(iL, N) * N); //http://math.stackexchange.com/questions/13261/how-to-get-a-reflection-vector
+   float alpha = max(0.0, dot(reflection, V)); // alpha like the one in the image on page 28 shading.pdf
+   result += mat.specular * light.specular * pow(alpha, mat.shininess);
+  //}
+ }
+ return result;
+}
 
 void main()
 {
-    gl_FragColor = gl_Color;
+	//gl_LightSourceParameters light = gl_LightSource[0];
+	gl_MaterialParameters mat = gl_FrontMaterial;
+	gl_FragColor = shading(P, N, mat);
 }
--- RobotRace/src/robotrace/shaderPrograms/Robot/vertex.glsl
+++ RobotRace/src/robotrace/shaderPrograms/Robot/vertex.glsl
@@ -1,7 +1,15 @@
-// simple vertex shader
+#version 120
+//uniform bool ambient, diffuse, specular;
+varying vec3 N, P;
 
-void main()
-{
-    gl_Position    = gl_ModelViewProjectionMatrix * gl_Vertex;      // model view transform
-    gl_FrontColor = gl_Color;
+void main() {
+ // pick up light LIGHT0 and material properties set in ShaderMaker
+ //gl_LightSourceParameters light = gl_LightSource[0];
+ //gl_MaterialParameters mat = gl_FrontMaterial;
+ N = normalize(gl_NormalMatrix * gl_Normal); // TODO: transform normal vector to view space
+ P = (gl_ModelViewMatrix * gl_Vertex).xyz / gl_Vertex.w; // TODO: compute vertex position in 3-D view coordinates
+ // output of vertex shader
+ gl_TexCoord[0] = gl_MultiTexCoord0;
+ //gl_FrontColor = shading(P, N, light, mat); // calculated in fragment
+ gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
 }
