c9317b70a0e65d814b23c2935e8388adb99fb438
Coen Stange <coen.stange@hotmail.com>
2016-12-26 00:51:54 +0100

    * make side panels

--- RobotRace/src/robotrace/RaceTrack.java
+++ RobotRace/src/robotrace/RaceTrack.java
@@ -27,7 +27,7 @@ abstract class RaceTrack {
      * Draws this track, based on the control points.
      */
     public void draw(GL2 gl, GLU glu, GLUT glut) {
-        //Vector lastVector1, lastVector2;
+        // render top part
         gl.glPushMatrix();
         for(int lane = 0; lane < 4; lane++)
         {
@@ -38,14 +38,49 @@ abstract class RaceTrack {
                 Vector vector = getLanePoint(lane, i);
                 Vector tangent = getLaneTangent(lane, i);
                 Vector normal = tangent.cross(Vector.Z).normalized();
-                Vector vector1 = getLanePoint(lane, i).add(normal.scale(laneWidth));
-                Vector vector2 = getLanePoint(lane, i).add(normal.scale(-laneWidth));
+                Vector vector1 = getLanePoint(lane, i).add(normal.scale(laneWidth/2));
+                Vector vector2 = getLanePoint(lane, i).add(normal.scale(-laneWidth/2));
                 gl.glVertex3d( vector2.x, vector2.y, vector2.z ); 
                 gl.glVertex3d( vector1.x, vector1.y, vector1.z ); 
 
             }
             gl.glEnd();
         }
+        gl.glPopMatrix();
+        //render inner side
+        gl.glPushMatrix();
+        //gl.glBegin(GL_LINE_STRIP);	
+        gl.glColor3d(1, 1, 0);
+        gl.glBegin(GL_TRIANGLE_STRIP);	
+
+        for(float i = 0; i <= 1.0f; i+= stepSize) {
+            Vector vector = getLanePoint(0, i);
+            Vector tangent = getLaneTangent(0, i);
+            Vector normal = tangent.cross(Vector.Z).normalized();
+            Vector vector1 = getLanePoint(0, i).add(normal.scale(-laneWidth/2));
+            gl.glVertex3d( vector1.x, vector1.y, vector1.z ); 
+            gl.glVertex3d( vector1.x, vector1.y, -1 ); 
+
+        }
+        gl.glEnd();
+        gl.glPopMatrix();
+        //render outside 
+        //render inner side
+        gl.glPushMatrix();
+        //gl.glBegin(GL_LINE_STRIP);	
+        gl.glColor3d(0, 1, 0);
+        gl.glBegin(GL_TRIANGLE_STRIP);	
+
+        for(float i = 0; i <= 1.0f; i+= stepSize) {
+            Vector vector = getLanePoint(3, i);
+            Vector tangent = getLaneTangent(3, i);
+            Vector normal = tangent.cross(Vector.Z).normalized();
+            Vector vector1 = getLanePoint(3, i).add(normal.scale(laneWidth/2));
+            gl.glVertex3d( vector1.x, vector1.y, vector1.z ); 
+            gl.glVertex3d( vector1.x, vector1.y, -1 ); 
+
+        }
+        gl.glEnd();
         gl.glPopMatrix();
     }
     
