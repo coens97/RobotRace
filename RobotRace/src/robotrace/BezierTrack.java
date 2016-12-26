46447d9de55a1615abfacee4bfdd2f4c32e39132
Coen Stange <coen.stange@hotmail.com>
2016-12-26 21:22:06 +0100

    * add getCubicBezierTng

--- RobotRace/src/robotrace/BezierTrack.java
+++ RobotRace/src/robotrace/BezierTrack.java
@@ -31,5 +31,23 @@ public class BezierTrack extends RaceTrack {
 
     }
     
-
+    public Vector getCubicBezierPnt(double t, Vector P0, Vector P1, Vector P2, Vector P3) {
+        double it = 1 - t;
+        return P0.scale(it*it*it)
+                .add(P1.scale(3*t*it*it))
+                .add(P2.scale(3*t*t*it))
+                .add(P3.scale(t*t*t));
+    }
+    
+    public Vector getCubicBezierTng(double t, Vector P0, Vector P1, Vector P2, Vector P3) {
+        double it = 1 - t;
+        return P1.subtract(P0)
+                .scale(3*it*it)
+                .add(
+                        P2.subtract(P1)
+                                .scale(6*it*t))
+                .add(
+                        P3.subtract(P2)
+                                .scale(3*t*t));
+    }
 }
